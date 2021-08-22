07434f3f6bffa9b3ce2542055898dcf2b99b0269
Margot <margot.guetta@appsflyer.com>
2021-08-22 20:53:04 +0300

    update to 6.3.5

--- Classes/Strict/AppsFlyer/libAppsFlyer/AppsFlyerLib.h
+++ Classes/Strict/AppsFlyer/libAppsFlyer/AppsFlyerLib.h
@@ -2,7 +2,7 @@
 //  AppsFlyerLib.h
 //  AppsFlyerLib
 //
-//  AppsFlyer iOS SDK 6.3.5.22 (22)
+//  AppsFlyer iOS SDK 6.3.5 (24)
 //  Copyright (c) 2012-2020 AppsFlyer Ltd. All rights reserved.
 //
 
