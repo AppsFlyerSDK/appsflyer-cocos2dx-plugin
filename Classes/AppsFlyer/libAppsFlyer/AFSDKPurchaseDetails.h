f8bf7797f0a4e9ca390bbc93f5bd17f209d0cc09
Ivan Obodovskyi <ivan.obodovskyi@appsflyer.com>
2025-11-11 16:18:47 +0200

    Update binaries and classes to v6.17.7 native ios SDK

--- Classes/AppsFlyer/libAppsFlyer/AFSDKPurchaseDetails.h
+++ Classes/AppsFlyer/libAppsFlyer/AFSDKPurchaseDetails.h
@@ -5,19 +5,20 @@
 //  Created by Moris Gateno on 13/03/2024.
 //
 
+#import <AppsFlyerLib/AFSDKPurchaseType.h>
+
 @interface AFSDKPurchaseDetails : NSObject
 
-- (nonnull instancetype)init NS_UNAVAILABLE;
-+ (nonnull instancetype)new NS_UNAVAILABLE;
+- (instancetype)init NS_UNAVAILABLE;
++ (instancetype)new NS_UNAVAILABLE;
 
-@property (strong, nullable, nonatomic) NSString *productId;
-@property (strong, nullable, nonatomic) NSString *price;
-@property (strong, nullable, nonatomic) NSString *currency;
-@property (strong, nullable, nonatomic) NSString *transactionId;
+@property (nonatomic, copy, readonly) NSString *productId;
+@property (nonatomic, copy, readonly) NSString *transactionId;
+@property (nonatomic, assign, readonly) AFSDKPurchaseType purchaseType;
 
-- (instancetype _Nonnull )initWithProductId:(NSString *_Nullable)productId
-                                      price:(NSString *_Nullable)price
-                                   currency:(NSString *_Nullable)currency
-                              transactionId:(NSString *_Nullable)transactionId;
+// Designated initializer for VAL2-compliant purchase object
+- (instancetype)initWithProductId:(NSString *)productId
+                   transactionId:(NSString *)transactionId
+                    purchaseType:(AFSDKPurchaseType)purchaseType NS_DESIGNATED_INITIALIZER;
 
 @end
